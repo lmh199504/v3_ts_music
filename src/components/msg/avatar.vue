34cc458482311a97bde9ff59ce81839ac160cf0b
lmh <1121376989@qq.com>
2022-07-14 18:10:32 +0800

    头像

---
+++ src/components/msg/avatar.vue
@@ -0,0 +1,15 @@
+<!-- 头像 -->
+<template>
+    <div class="avatar">
+        <img src="" alt="">
+    </div>
+</template>
+<script setup lang="ts">
+interface Props{
+    avatarUrl: string
+}
+const props = withDefaults(defineProps<Props>(), {
+    avatarUrl: ''
+})
+
+</script>
